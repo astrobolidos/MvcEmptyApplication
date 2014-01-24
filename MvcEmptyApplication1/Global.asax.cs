abfeb26a5c60d296f8ab0bf05d594e4793e018a6
Lisandro Pacheco <lisandroalavez@gmail.com>
2014-01-24 10:15:47 +0000

    adding link to documentation

--- MvcEmptyApplication1/Global.asax.cs
+++ MvcEmptyApplication1/Global.asax.cs
@@ -6,7 +6,7 @@ namespace MvcEmptyApplication1
 {
     // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
     // visit http://go.microsoft.com/?LinkId=9394801
-
+    // visit: http://www.codeproject.com/Articles/615805/Creating-a-Clean-Minimal-Footprint-ASP-NET-WebAPI
     public class WebApiApplication : HttpApplication
     {
         protected void Application_Start()
